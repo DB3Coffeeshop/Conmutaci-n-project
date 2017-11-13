2016ed20c9c55c73081c7abbe208c8a87f11bd04
Zandex <zowashn@gmail.com>
2017-11-12 22:52:56 -0500

    Cmamut tree

--- scripts/control_panel.py
+++ scripts/control_panel.py
@@ -11,9 +11,7 @@ class App(Frame):
         self.master = master
         self.make_frame()
         self.student_code_text = Entry(self.master)
-        self.pass_text = Entry(self.master, show="*")
         self.student_code_text.place(relx=0.5,rely=0.1)
-        self.pass_text.place(relx=0.3, rely=0.4)
         
     def make_frame(self):
         #LABELS
@@ -25,14 +23,28 @@ class App(Frame):
         #ComboBox
         self.category_first_Cbox = Combobox(self.master, state="readonly")
         self.category_second_Cbox = Combobox(self.master, state="readonly")
-        self.category_first_Cbox['values'] = ['Pulga', 'Marika', 'Soplon']
+        self.category_first_Cbox['values'] = ['','Pulga', 'Marika', 'Soplon']
         self.category_second_Cbox['values'] = ['Marika', 'Es', 'Pulga']
 
         #BUTTOMS
         button_search = Button(self.master, text="Search", highlightbackground='#17202a', command=self.search)
         button_back = Button(self.master, text="Atras", highlightbackground='#17202a', command=self.back)
         button_exit = Button(self.master, text="Exit", highlightbackground='#17202a', command=self.exit)
+        button_moreinf = Button(self.master, text="More inf", highlightbackground='#17202a', command=self.selected_tree)
 
+        #TREEVIEW
+        self.grid()
+        tree = self.tree = Treeview(self,columns=('Code Student','Material','Date','Return'),show="headings",selectmode='browse')
+        tree.heading("Code Student", text="Code Student")
+#        tree.column('Code Studen',anchor='center',width=30)
+        tree.heading("Material", text="Material")
+        tree.heading("Date",text="Date")
+        tree.heading("Return",text="Return")
+        tree.grid(padx = 5,pady=5)
+        #tree.grid(place=2)
+
+        i = tree.insert('','end',values = ['0353','567','adwa','adwad'])
+        tree.insert(i,'end',values = ['0353','567','adwa','adwad'])
         
         #Pociciones
         title.place(relx=0.01,rely=0.01)
@@ -45,8 +57,18 @@ class App(Frame):
 
         button_search.place(relx=0.75, rely=0.065)
         button_back.place(relx=0.75, rely=0.12)
-        button_exit.place(relx=0.6, rely=0.7)
+        button_moreinf.place(relx=0.75, rely=0.175)
+        button_exit.place(relx=0.75, rely=0.23)
 
+        #tree.place(relx=0.1,rely=0.2)
+
+    def selected_tree(self):
+        curItem = self.tree.focus();
+        print self.tree.item(curItem)['values'][0]
+        self.quit()
+
+    def remove_all(self):
+        pass
 
     def exit(self):
         answer = tkMessageBox.askquestion("Exit", "Are you sure?")
@@ -55,29 +77,28 @@ class App(Frame):
             self.master.destroy()
 
     def search(self):
+        item=""
+        student_code=self.student_code_text.get()
         self.category_first_Cbox['values'] = ['ASDAW', 'dwada', 'dwadwa']
+
+        if item != "":
+            if student_code != "":
+                #Enviar codigo con ambos requisitos
+            #else:
+                #Perdir solo por item
+                pass
+
+        #else:
+            if student_code != "": 
+                #pedir por codigo          
+            #else:
+                tkMessageBox.showinfo("Error", "Select one")
+                pass
+
 
     def back(self):
         self.master.destroy()
         main = Main()
-
-    def ololo(self):
-        if self.user_text.get() == "andres" and self.pass_text.get() == "12345":
-            tkMessageBox.showinfo("Correct", "Welcome {0}".format(getpass.getuser()))
-            self.master.destroy()
-            main = Main()
-        else:
-            if self.user_text.get() == "andres" and self.pass_text.get() != "12345":
-                tkMessageBox.showerror("Error", "Invalid password")
-            
-            elif self.user_text.get() != "andres" and self.pass_text.get() == "12345":
-                tkMessageBox.showerror("Error", "Invalid user")
-
-            else:
-                tkMessageBox.showinfo("Error", "Invalid user and password")
-
-        
-
 
 
 if __name__ == "__main__":
