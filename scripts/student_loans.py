89fa4e1f7a266cc92cefb5568b4b195c340bec71
Andres <pulga1021@outlook.es>
2017-11-19 17:36:50 -0500

    script student_loans complete , I still have to fix the cancel button

--- scripts/student_loans.py
+++ scripts/student_loans.py
@@ -5,6 +5,7 @@ import tkMessageBox
 from article import Article
 from datetime import date
 
+
 class Loans:
 
     def __init__(self, student):
@@ -16,12 +17,15 @@ class Loans:
         self.configure()
         self.make_list_articles()
         self.make_loans_frame()
+        self.student_loans_list()
         self.master.mainloop()
 
 
     def configure(self):
         self.master.title("Loans")
-        title = Label(self.master, text="%s %s" % (self.student.name, self.student.last_name), bg='#dbe0df', font=("Cursive", 19))
+        self.head = StringVar()
+        title = Label(self.master, textvariable=self.head, bg='#dbe0df', font=("Cursive", 19))
+        self.head.set("%s %s" % (self.student.name, self.student.last_name))
         title.pack()
         self.article_frame.pack(side=RIGHT, padx=30)
         self.loans_frame.pack(side=LEFT, padx=30)
@@ -47,11 +51,11 @@ class Loans:
         self.list = Listbox(self.article_frame, selectborderwidth=5)
 
         title = Label(self.article_frame, text="Articles", bg='#dbe0df')
-        btn_accept = Button(self.article_frame, text="Add", highlightbackground='#dbe0df', command=self.add_button)
+        self.btn_add = Button(self.article_frame, text="Add", highlightbackground='#dbe0df', command=self.add_button)
         lbl_quantity = Label(self.article_frame, text="quantity", font=("Cursive", 10), bg='#dbe0df')
         self.txt_quantity = Entry(self.article_frame, bd=5, text="Quantity here")
 
-        btn_accept.pack(side=BOTTOM)
+        self.btn_add.pack(side=BOTTOM)
         self.txt_quantity.pack(side=BOTTOM, pady=10)
         lbl_quantity.pack(side=BOTTOM, pady=10)
         title.pack()
@@ -75,17 +79,17 @@ class Loans:
         self.list_loans.pack(side=LEFT, padx=30)
         self.list_new_loans.pack(side=RIGHT, padx=30, pady=30)
 
-        btn_accept = Button(self.master, text="Accept", highlightbackground='#dbe0df', width=10, command=self.accept_button)
-        btn_cancel = Button(self.master, text="Cancel", highlightbackground='#dbe0df', width=10)
-        btn_exit = Button(self.master, text="exit", highlightbackground='#dbe0df', width=10)
+        self.btn_accept = Button(self.master, text="Accept", highlightbackground='#dbe0df', width=10, command=self.accept_button)
+        self.btn_cancel = Button(self.master, text="Cancel", highlightbackground='#dbe0df', width=10, command=self.cancel_button)
+        btn_exit = Button(self.master, text="exit", highlightbackground='#dbe0df', width=10, command=self.go_back)
 
         lbl_current_loans = Label(self.master, text="Student Loans", bg='#dbe0df')
         lbl_loans = Label(self.master, text="Current loans", bg='#dbe0df')
 
         lbl_current_loans.place(relx=0.12, rely=0.2)
         lbl_loans.place(relx=0.41, rely=0.2)
-        btn_accept.place(relx=0.07, rely=0.9)
-        btn_cancel.place(relx=0.27, rely=0.9)
+        self.btn_accept.place(relx=0.07, rely=0.9)
+        self.btn_cancel.place(relx=0.27, rely=0.9)
         btn_exit.place(relx=0.47, rely=0.9)
 
 
@@ -123,8 +127,9 @@ class Loans:
 
 
     def accept_button(self):
+        db = self.connect()
+
         if len(self.list_articles) > 0:
-            db = self.connect()
             cursor = db.cursor()
 
             for article in self.list_articles:
@@ -139,11 +144,54 @@ class Loans:
                     sql = "INSERT INTO Loan(student_code, material, cant, date, status) VALUES(%d, %d, %d, '%s', %d)" % (student_code, id_material, int(article[1]), date.today(), 1)
                     cursor.execute(sql)
 
+                    sql_stock = "UPDATE Materials SET stock = stock - %d WHERE id_Materials=%d" % (int(article[1]), id_material)
+                    cursor.execute(sql_stock)
+
                 except:
                     tkMessageBox.showerror("Error", "Unexpected error")
 
         self.fill_list_loans()
+        self.success()
         db.close()
+
+
+    def success(self):
+        self.head.set("Success")
+        self.btn_add['state'] = 'disabled'
+        self.btn_accept['state'] = 'disabled'
+        self.btn_cancel['state'] = 'disabled'
+    
+    
+    def student_loans_list(self):
+        data_base = self.connect()
+        cursor = data_base.cursor()
+        sql = "SELECT Materials.name FROM Loan INNER JOIN Materials ON Loan.material = Materials.id_Materials WHERE student_code=%d" % (int(self.student.stundent_code))
+        cursor.execute(sql)
+        data = cursor.fetchall()
+
+        for current in range(len(data)):
+            self.list_loans.insert(END, data[current][0])
+
+        data_base.close()
+
+
+    def go_back(self):
+        from loans import Register
+        if len(self.list_articles) > 0:
+            answer = tkMessageBox.askquestion("Exit", "Are you sure")
+
+            if answer == 'yes':
+                self.master.destroy()
+                student_frame = Register()
+
+        else:
+            self.master.destroy()
+            student_frame = Register()
+
+
+    def cancel_button(self):
+        self.list_new_loans.delete(0, END)
+        self.list_articles = []
 
 
 
