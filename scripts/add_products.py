e13057f8023824b8c30476ff120581364df6a427
Andres <pulga1021@outlook.es>
2017-11-23 17:17:20 -0500

    data base add_products complete

--- scripts/add_products.py
+++ scripts/add_products.py
@@ -30,24 +30,24 @@ class App(Frame):
 
         #OTHERS
         Id_producto_str = StringVar()
-        id_entry = Entry(root,textvariable=Id_producto_str)
+        self.id_entry = Entry(root,textvariable=Id_producto_str)
         #2
         Categoria_1 = StringVar()
-        last_entry = Entry(root,textvariable=Categoria_1)
+        self.last_entry = Entry(root,textvariable=Categoria_1)
         #3
         Categoria_2 = StringVar()
-        mail_entry = Entry(root,textvariable=Categoria_2)
+        self.mail_entry = Entry(root,textvariable=Categoria_2)
         #4
         Nombre_str = StringVar()
-        nombre_entry = Entry(root,textvariable=Nombre_str)
+        self.nombre_entry = Entry(root,textvariable=Nombre_str)
         #5
         Stock_str = StringVar()
-        stock_entry = Entry(root,textvariable=Stock_str)
+        self.stock_entry = Entry(root,textvariable=Stock_str)
     
         
 
         #BUTTOMS
-        button_agregar = Button(self.master, text="Agregar", highlightbackground='#17202a')
+        button_agregar = Button(self.master, text="Agregar", highlightbackground='#17202a', command=self.add_product)
         button_back = Button(self.master, text="Atras", highlightbackground='#17202a', command=self.back)
         button_exit = Button(self.master, text="Exit", highlightbackground='#17202a', command=self.exit)
         
@@ -60,15 +60,15 @@ class App(Frame):
 
         #Pociciones
         id_label.place(relx=0.08,rely=0.10)
-        id_entry.place(relx=0.08,rely=0.15)
+        self.id_entry.place(relx=0.08,rely=0.15)
         last_label.place(relx=0.08,rely=0.25)
-        last_entry.place(relx=0.08,rely=0.30)
+        self.last_entry.place(relx=0.08,rely=0.30)
         mail_label.place(relx=0.08,rely=0.40)
-        mail_entry.place(relx=0.08,rely=0.45)
+        self.mail_entry.place(relx=0.08,rely=0.45)
         nombre_label.place(relx=0.08,rely=0.55)
-        nombre_entry.place(relx=0.08,rely=0.60)
+        self.nombre_entry.place(relx=0.08,rely=0.60)
         stock_label.place(relx=0.08,rely=0.70)
-        stock_entry.place(relx=0.08,rely=0.75)
+        self.stock_entry.place(relx=0.08,rely=0.75)
 
         button_agregar.place(relx=0.45, rely=0.45)
         button_back.place(relx=0.55, rely=0.45)
@@ -76,14 +76,40 @@ class App(Frame):
 
         #DESCOMENTE ESTOOOOOO TAMBIEN
         button_stundents.pack(padx=100, pady=30)
-
-    def db_execute(self,texts):
-        self.cur.execute(texts)
-        return self.cur.fetchall()
             
+
     def conectar(self):
-        self.db = MySQLdb.connect("LocalHost", "root", "natalia1", "Eafit_Loans") #Ignoren la contra, no supe como cambiarla
-        self.cur = self.db.cursor()
+        db = MySQLdb.connect("LocalHost", "root", "natalia1", "Eafit_Loans") #Ignoren la contra, no supe como cambiarla
+        db.autocommit(True)
+        return db
+
+
+    def add_product(self):
+        data_base = self.conectar()
+        cursor = data_base.cursor()
+        sql = "INSERT INTO Materials(id_Materials, category_first, category_second, name, stock) VALUES(%d, '%s', '%s', '%s', '%d')" % (int(self.id_entry.get()), self.last_entry.get(), self.mail_entry.get(), self.nombre_entry.get(),  int(self.stock_entry.get())) 
+
+        try:
+            cursor.execute(sql)
+            tkMessageBox.showinfo("Success", "Product has been added")
+            self.clean()
+
+        except MySQLdb.IntegrityError:
+            tkMessageBox.showerror("Error", "Product with id %s already exists") % (self.id_entry.get())
+        
+        except:
+            tkMessageBox.showerror("Error", "Unexpected error")
+
+        data_base.close()
+
+
+    def clean(self):
+        self.id_entry.delete(0, END)
+        self.last_entry.delete(0, END)
+        self.mail_entry.delete(0, END)
+        self.nombre_entry.delete(0, END)
+        self.stock_entry.delete(0, END)
+
 
     def exit(self):
         answer = tkMessageBox.askquestion("Exit", "Are you sure?")
